b0b1b2cb6b2c9999dec4ab6f086e04b0a146ae52
JoelLindow <joellindow@gmail.com>
2017-05-16 12:51:56 -0600

    broken multiple allergy method. must fix

--- allergy_test/lib/allergies.rb
+++ allergy_test/lib/allergies.rb
@@ -3,12 +3,27 @@ require 'pry'
 
 
 class Allergies
-  attr_reader :score, :potential_allergies
-  
-  def initialize(score)
-    @score = score
-    @potential_allergies = AllergenLibrary.new
-    # binding.pry
+  attr_reader :score, :patient_name, :potential_allergies, :patient_allergies
+
+  def initialize(patient_name, score)
+    @patient_name         = patient_name
+    @score                = score
+    @potential_allergies  = AllergenLibrary.new
+    @patient_allergies    = []
+  end
+
+  def find_allergens
+    potential_allergies.allergens.keys.sort.reverse.each do |num|
+      until @score == 0
+        # binding.pry
+        if num < @score
+          @patient_allergies << potential_allergies.allergens[num]
+          @score -= num
+        elsif num == @score
+          @patient_allergies << potential_allergies.allergens[num]
+        end
+      end
+    end
   end
 
 end
--- allergy_test/test/allergies_test.rb
+++ allergy_test/test/allergies_test.rb
@@ -5,20 +5,33 @@ require 'pry'
 
 class AllergiesTest < Minitest::Test
 
-  def test_we_have_a_class_called_allergies
-  patient = Allergies.new(128)
-  assert_instance_of Allergies, patient
-  end
-
-  def test_it_can_return_an_allergen
-  patient = Allergies.new(128)
-
-  assert_equal "cats", patient.potential_allergies.allergens[patient.score]
-  end
-
+  # def test_we_have_a_class_called_allergies
+  # patient = Allergies.new("Joel", 128)
+  # assert_instance_of Allergies, patient
+  # end
+  #
+  # def test_it_knows_patients_name
+  #   patient = Allergies.new("Joel", 128)
+  #
+  #   assert_equal "Joel", patient.patient_name
+  # end
+  #
+  # def test_it_can_return_an_allergen
+  # patient = Allergies.new("Joel", 128)
+  #
+  # assert_equal "cats", patient.potential_allergies.allergens[patient.score]
+  # end
+  #
   def test_it_can_return_another_allergen
-  patient = Allergies.new(64)
+  patient = Allergies.new("Joel", 64)
 
   assert_equal "pollen", patient.potential_allergies.allergens[patient.score]
+  end
+
+  def test_it_can_return_multiple_allergens
+  patient = Allergies.new("Joel", 3)
+  patient.find_allergens
+
+  assert_equal [], patient.patient_allergies
   end
 end
